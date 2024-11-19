ebc64b44d47b744e06be52f96ade92213d8ff5a1
Tang2812 <dinhvietnhattan@gmail.com>
2024-11-19 17:20:48 +0700

    feat: add modal to view table result and javascript to handle get day today, open/close modal

--- practice/src/js/main.js
+++ practice/src/js/main.js
@@ -16,6 +16,25 @@ function activeButton(index) {
 const slider = document.querySelector('#slider');
 const sliderValue = document.querySelector('#sliderValue');
 
-console.log(slider.value)
-slider.addEventListener('input', () => { sliderValue.textContent = slider.value; })
+console.log(slider.value);
+slider.addEventListener('input', () => { sliderValue.textContent = slider.value + '%'; });
 
+// open and close modal
+function openCloseModal() {
+  const modal = document.querySelector('.modal-container');
+  modal.classList.toggle('modal--visible')
+}
+
+// get date today
+function getDayToDay() {
+  const inputBox = document.querySelector('#disbursement-date')
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  const result = `${day}/${month}/${year}`;
+  inputBox.value = result;
+  console.log(result);
+}
+
+getDayToDay();
